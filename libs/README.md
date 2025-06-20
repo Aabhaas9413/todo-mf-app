9604bc465e0ca6b5481337ae9f53d39d22129877
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-06-20 21:31:03 +0100

    adding remotes and shared lib.

---
+++ libs/README.md
@@ -0,0 +1,3 @@
+# shared-communication
+
+This library was generated with [Nx](https://nx.dev).
---
+++ libs/src/lib/shared-communication/shared-communication.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-shared-communication',
+  imports: [CommonModule],
+  templateUrl: './shared-communication.html',
+  styleUrl: './shared-communication.css',
+})
+export class SharedCommunication {}
